56c42cd0995b4789933bfad5a688f242a842147d
Bohdan Wazer <ki4ack.bogdan@gmail.com>
2023-11-06 20:54:45 +0200

    1234

--- main.js
+++ main.js
@@ -1,58 +1,141 @@
-document
-  .getElementById('optionalFieldsButton')
-  .addEventListener('click', function () {
-    const optionalFields = document.querySelector('.optional-fields');
-    optionalFields.style.display =
-      optionalFields.style.display === 'none' ? 'block' : 'none';
-  });
+// Функція для отримання курсів валют з API Приватбанку
+function getExchangeRates() {
+  return fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
+    .then((response) => response.json())
+    .then((data) => {
+      // Знайдіть курси долара та євро у відповіді API
+      const usdRate = data.find((currency) => currency.ccy === 'USD');
+      const eurRate = data.find((currency) => currency.ccy === 'EUR');
 
-function calculatePension() {
+      // Отримайте курси купівлі та продажу для долара та євро
+      const usdBuyRate = parseFloat(usdRate.buy);
+      const usdSaleRate = parseFloat(usdRate.sale);
+      const eurBuyRate = parseFloat(eurRate.buy);
+      const eurSaleRate = parseFloat(eurRate.sale);
+
+      return { usdBuyRate, usdSaleRate, eurBuyRate, eurSaleRate };
+    })
+    .catch((error) => {
+      console.error('Помилка при отриманні курсів валют: ', error);
+      return null;
+    });
+}
+
+// Функція для розрахунку пенсійного капіталу з урахуванням курсів валют
+async function calculatePension() {
+  // Отримайте курси валют з API Приватбанку
+  const exchangeRates = await getExchangeRates();
+
+  if (exchangeRates === null) {
+    return;
+  }
+
+  // Отримайте значення введених користувачем даних
   const initialContribution = parseFloat(
     document.getElementById('initial_contribution').value
   );
-  const annualContribution = parseFloat(
-    document.getElementById('annual_contribution').value
+  const monthlyContribution = parseFloat(
+    document.getElementById('monthly_contribution').value
+  );
+  const annualInterestRate = parseFloat(
+    document.getElementById('annual_interest_rate').value
+  );
+  const years = parseInt(document.getElementById('years').value);
+  const additionalContributions = parseFloat(
+    document.getElementById('additional_contributions').value
+  );
+  const inflationRate = parseFloat(
+    document.getElementById('inflation_rate').value
+  );
+  const showEveryThreeYears = document.getElementById(
+    'show_every_three_years'
+  ).checked;
+  const convertToCurrency = document.getElementById(
+    'convert_to_currency'
+  ).value;
+  const additionalContributionsYear = parseInt(
+    document.getElementById('additional_contributions_year').value
   );
 
-  // Отримання додаткових даних, якщо вони відображені
-  let annualInterestRate = 0;
-  let years = 0;
-  if (document.querySelector('.optional-fields').style.display === 'block') {
-    annualInterestRate =
-      parseFloat(document.getElementById('annual_interest_rate').value) / 100;
-    years = parseInt(document.getElementById('years').value);
+  // Перевіряємо, чи коректно введені всі значення
+  if (
+    isNaN(initialContribution) ||
+    isNaN(monthlyContribution) ||
+    isNaN(annualInterestRate) ||
+    isNaN(years) ||
+    isNaN(additionalContributions) ||
+    isNaN(inflationRate)
+  ) {
+    alert('Будь ласка, введіть всі необхідні дані.');
+    return;
   }
 
-  const futureValues = [initialContribution];
+  // Конвертуємо річну ставку в щомісячну
+  const monthlyInterestRate = (1 + annualInterestRate / 100) ** (1 / 12) - 1;
 
-  for (let year = 1; year <= years; year++) {
-    futureValues.push(futureValues[year - 1] + annualContribution);
-    futureValues[year] *= 1 + annualInterestRate;
+  // Обчислюємо пенсійний капітал та збитки від інфляції
+  let pensionCapital = initialContribution;
+  let totalInflationLoss = 0;
+  const capitalByYear = [];
+  const additionalContributionsMap = new Map();
+
+  for (let i = 1; i <= years * 12; i++) {
+    if (additionalContributionsYear === i / 12) {
+      pensionCapital += additionalContributions;
+      additionalContributionsMap.set(i / 12, additionalContributions);
+    }
+    pensionCapital += monthlyContribution;
+    pensionCapital *= 1 + monthlyInterestRate;
+
+    if (i % 12 === 0 || i === years * 12) {
+      capitalByYear.push({ year: i / 12, capital: pensionCapital });
+    }
+
+    // Рахуємо збитки від інфляції щомісяця
+    const inflationLoss = (pensionCapital * (inflationRate / 100)) / 12;
+    totalInflationLoss += inflationLoss;
+    pensionCapital -= inflationLoss;
   }
 
-  if (chart) {
-    chart.destroy();
+  // Обчислюємо результати в обраній валюті
+  let resultText = '';
+  if (convertToCurrency === 'USD') {
+    const pensionInUSD = pensionCapital / exchangeRates.usdBuyRate;
+    resultText = `Ваш пенсійний капітал через ${years} років становитиме: ${pensionInUSD.toFixed(
+      2
+    )} USD<br>`;
+  } else if (convertToCurrency === 'EUR') {
+    const pensionInEUR = pensionCapital / exchangeRates.eurBuyRate;
+    resultText = `Ваш пенсійний капітал через ${years} років становитиме: ${pensionInEUR.toFixed(
+      2
+    )} EUR<br>`;
+  } else {
+    resultText = `Ваш пенсійний капітал через ${years} років становитиме: ${pensionCapital.toFixed(
+      2
+    )} грн<br>`;
   }
 
-  const ctx = document.getElementById('pensionChart').getContext('2d');
+  // Відображаємо результат на сторінці
+  const resultElement = document.getElementById('result');
+  resultElement.innerHTML = resultText;
 
-  chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: Array.from({ length: years + 1 }, (_, i) => i),
-      datasets: [
-        {
-          label: 'Пенсійний капітал',
-          data: futureValues,
-          borderColor: 'blue',
-          borderWidth: 2,
-          fill: false,
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    },
+  // Відображаємо стан капіталу за кожний рік
+  const capitalByYearElement = document.getElementById('capital_by_year');
+  capitalByYearElement.innerHTML = `<p>Стан капіталу за кожний рік:</p>`;
+  capitalByYear.forEach((item) => {
+    capitalByYearElement.innerHTML += `Рік ${item.year}: ${item.capital.toFixed(
+      2
+    )} грн<br>`;
+  });
+
+  // Відображаємо додаткові внески за роками
+  const additionalContributionsElement = document.getElementById(
+    'additional_contributions_list'
+  );
+  additionalContributionsElement.innerHTML = `<p>Додаткові внески:</p>`;
+  additionalContributionsMap.forEach((value, year) => {
+    additionalContributionsElement.innerHTML += `Рік ${year}: ${value.toFixed(
+      2
+    )} грн<br>`;
   });
 }
